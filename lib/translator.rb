0ea1a3ec9c66218d3b9b52b38a9f734ec9a4bfd9
Shakore McGill <mcgills2093@gmail.com>
2020-05-13 16:47:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,19 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  organized_emojis = {}
+
+  emojis.each do |key, value|
+    
+    if !organized_emojis.include?(key)
+      organized_emojis[key] = {}
+    end
+
+    if !organized_emojis[key].include?(value[0])
+        organized_emojis[key] = {english: value[0], japanese: value[1]}
+    end
+  end 
+  return organized_emojis
 end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
