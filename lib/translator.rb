4b15588ea07dbd6df0855ea35a75f503f275c3eb
Shakore McGill <mcgills2093@gmail.com>
2020-05-13 16:58:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,9 +3,9 @@ require "yaml"
 def load_library(file)
 emoticons = YAML.load_file(file)
 emojis = {}
-emoticons.each do |mood, value|
-  if !emojis.include?(mood)
+  emoticons.each do |mood, value|
+    if !emojis.include?(mood)
     emojis[mood] = {}
+    end
   end
 end
-
