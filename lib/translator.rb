761b93fc679f9a593ee0dceee29bbc1207af0903
Shakore McGill <mcgills2093@gmail.com>
2020-05-13 16:53:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,19 +1,6 @@
 require "yaml"
 
 def load_library(file)
-  emojis = YAML.load_file(file)
-  organized_emojis = {}
 
-  emojis.each do |key, value|
-    
-    if !organized_emojis.include?(key)
-      organized_emojis[key] = {}
-    end
-
-    if !organized_emojis[key].include?(value[0])
-        organized_emojis[key] = {english: value[0], japanese: value[1]}
-    end
-  end 
-  return organized_emojis
 end
 
